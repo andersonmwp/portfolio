bffc4b006224dfe88fbcf6d39a5b0d80dd65ea85
Anderson <andersonmwp@gmail.com>
2022-07-20 20:32:17 -0300

    melhorias no menu

--- src/js/btnMenu.js
+++ src/js/btnMenu.js
@@ -1,6 +1,34 @@
-const $menu = document.querySelector('.menu')
-const $btnMenu = document.querySelector('.btnMenu')
+//Evento abre e fecha do menu hamburguer
+const menuBurger = document.querySelector('.burger');
 
-$btnMenu.addEventListener('click', function(){
-    $menu.classList.toggle('menu_open')
-})
+function menuActive() {
+    menuBurger.classList.toggle('active');
+}
+
+menuBurger.addEventListener('click', menuActive);
+
+// Abrir e fechar navbar
+const btnMenu = document.querySelector('.menu__mobile .burger');
+
+function openMenu() {
+    const menuMobile = document.querySelector('.menu');
+
+    menuMobile.classList.toggle('menu_open');
+}
+
+btnMenu.addEventListener('click', openMenu);
+
+// Remover as classes para fechar o menu ao clicar no link
+const menuLinks = document.querySelectorAll('.menu__mobile-item a');
+
+function closeMenu() {
+    const menuBurger = document.querySelector('.burger');
+    const menuMobile = document.querySelector('.menu');
+
+    menuBurger.classList.remove('active');
+    menuMobile.classList.remove('menu_open');
+}
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', closeMenu);
+});
