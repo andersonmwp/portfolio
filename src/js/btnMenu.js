59414ac47d2f623bf2a214c8956377cf42d3ea78
Anderson <andersonmwp@gmail.com>
2022-07-20 21:50:34 -0300

    isolando em funções

--- src/js/btnMenu.js
+++ src/js/btnMenu.js
@@ -1,34 +1,44 @@
-//Evento abre e fecha do menu hamburguer
-const menuBurger = document.querySelector('.burger');
-
-function menuActive() {
-    menuBurger.classList.toggle('active');
-}
-
-menuBurger.addEventListener('click', menuActive);
-
-// Abrir e fechar navbar
-const btnMenu = document.querySelector('.menu__mobile .burger');
-
-function openMenu() {
-    const menuMobile = document.querySelector('.menu');
-
-    menuMobile.classList.toggle('menu_open');
-}
-
-btnMenu.addEventListener('click', openMenu);
-
-// Remover as classes para fechar o menu ao clicar no link
-const menuLinks = document.querySelectorAll('.menu__mobile-item a');
-
-function closeMenu() {
+function initAnimateMenu() {
+    //Evento abre e fecha do menu hamburguer
     const menuBurger = document.querySelector('.burger');
-    const menuMobile = document.querySelector('.menu');
 
-    menuBurger.classList.remove('active');
-    menuMobile.classList.remove('menu_open');
+    function menuActive() {
+        menuBurger.classList.toggle('active');
+    }
+
+    menuBurger.addEventListener('click', menuActive);
 }
 
-menuLinks.forEach((link) => {
-    link.addEventListener('click', closeMenu);
-});
+function initOpenMenu() {
+    // Abrir e fechar navbar
+    const btnMenu = document.querySelector('.menu__mobile .burger');
+
+    function openMenu() {
+        const menuMobile = document.querySelector('.menu');
+
+        menuMobile.classList.toggle('menu_open');
+    }
+
+    btnMenu.addEventListener('click', openMenu);
+}
+
+function initCloseMenu() {
+    // Remover as classes para fechar o menu ao clicar no link
+    const menuLinks = document.querySelectorAll('.menu__mobile-item a');
+
+    function closeMenu() {
+        const menuBurger = document.querySelector('.burger');
+        const menuMobile = document.querySelector('.menu');
+
+        menuBurger.classList.remove('active');
+        menuMobile.classList.remove('menu_open');
+    }
+
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', closeMenu);
+    });
+}
+
+initAnimateMenu();
+initOpenMenu();
+initCloseMenu();
--- src/js/scrollSmooth.js
+++ src/js/scrollSmooth.js
@@ -1,47 +1,57 @@
-const linksInternos = document.querySelectorAll('.menuNav a[href^="#"]');
+function initScrollSection() {
+	const linksInternos = document.querySelectorAll('.menuNav a[href^="#"]');
 
-function scrollSection(event) {
-	event.preventDefault();
-	const href = event.currentTarget.getAttribute('href');
-	const section = document.querySelector(href);
+	function scrollSection(event) {
+		event.preventDefault();
+		const href = event.currentTarget.getAttribute('href');
+		const section = document.querySelector(href);
+		
+		section.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+	}
 	
-	section.scrollIntoView({
-		behavior: 'smooth',
-		block: 'start',
+	linksInternos.forEach((link) => {
+		link.addEventListener('click', scrollSection);
 	});
 }
 
-linksInternos.forEach((link) => {
-	link.addEventListener('click', scrollSection);
-});
+function initScrollAbertura() {
+	const linkAbertura = document.querySelector('.abertura a[href^="#"]');
 
-const linkAbertura = document.querySelector('.abertura a[href^="#"]');
-
-function scrollInit(event){
-	event.preventDefault();
-	const href = event.currentTarget.getAttribute('href');
-	const section = document.querySelector(href);
+	function scrollInit(event){
+		event.preventDefault();
+		const href = event.currentTarget.getAttribute('href');
+		const section = document.querySelector(href);
+		
+		section.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+	}
 	
-	section.scrollIntoView({
-		behavior: 'smooth',
-		block: 'start',
-	});
+	linkAbertura.addEventListener('click', scrollInit);
 }
 
-linkAbertura.addEventListener('click', scrollInit);
-
-const sections = document.querySelectorAll('.js-scroll');
-const windowHalf = innerHeight * 0.75;
-
-function animateScroll() {
-	sections.forEach((section) => {
-		const sectionTop = section.getBoundingClientRect().top - windowHalf;
-		if(sectionTop < 0) {
-			section.classList.add('active');
-		} else {
-			section.classList.remove('active');
-		}
-	});
+function initScrollWindow() {
+	const sections = document.querySelectorAll('.js-scroll');
+	const windowHalf = innerHeight * 0.75;
+	
+	function animateScroll() {
+		sections.forEach((section) => {
+			const sectionTop = section.getBoundingClientRect().top - windowHalf;
+			if(sectionTop < 0) {
+				section.classList.add('active');
+			} else {
+				section.classList.remove('active');
+			}
+		});
+	}
+	
+	window.addEventListener('scroll', animateScroll);
 }
 
-window.addEventListener('scroll', animateScroll);
+initScrollSection();
+initScrollAbertura();
+initScrollWindow();
